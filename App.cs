d6bb75ba6088810bb2cf643de9d0babb9fb41443
Martin Espadas Jr <bobey90@gmail.com>
2025-01-09 13:42:03 -0800

    Working code, Still need to add icons

--- App.cs
+++ App.cs
@@ -28,7 +28,7 @@ namespace RVTBootcamp_Module04_Challenge
 
             RibbonPanel panel = app.CreateRibbonPanel(tabName, panelName);
 
-            //2. Create Button Data 1-10
+            
             PushButtonData buttonData1 = new PushButtonData("cmd1", "Tool 1", Assembly.GetExecutingAssembly().Location, "RVTBootcamp_Module04_Challenge.cmd1");
             PushButtonData buttonData2 = new PushButtonData("cmd2", "Tool 2", Assembly.GetExecutingAssembly().Location, "RVTBootcamp_Module04_Challenge.cmd2");
             PushButtonData buttonData3 = new PushButtonData("cmd3", "Tool 3", Assembly.GetExecutingAssembly().Location, "RVTBootcamp_Module04_Challenge.cmd3");
@@ -40,22 +40,41 @@ namespace RVTBootcamp_Module04_Challenge
             PushButtonData buttonData9 = new PushButtonData("cmd9", "Tool 9", Assembly.GetExecutingAssembly().Location, "RVTBootcamp_Module04_Challenge.cmd9");
             PushButtonData buttonData10 = new PushButtonData("cmd10", "Tool 10", Assembly.GetExecutingAssembly().Location, "RVTBootcamp_Module04_Challenge.cmd10");
 
-            //6. Create push Button
+            //Bitmap myBitmap = Properties.Resources.Green_32; // loading a Bitmap from resources
+            ////byte[] byteArray = ConvertBitmapToByteArray(myBitmap);
+
+            //Bitmap myBitmap2 = Properties.Resources.Green_16; 
+            ////byte[] byteArray2 = ConvertBitmapToByteArray(myBitmap2);
+
+            //Bitmap myBitmap3 = Properties.Resources.Blue_32; 
+            ////byte[] byteArray3 = ConvertBitmapToByteArray(myBitmap3);
+
+            //Bitmap myBitmap4 = Properties.Resources.Blue_16; 
+            ////byte[] byteArray4 = ConvertBitmapToByteArray(myBitmap4);
+
+            
 
             PushButton button1 = panel.AddItem(buttonData1) as PushButton;
             PushButton button2 = panel.AddItem(buttonData2) as PushButton;
 
-            //7. Add stackable buttons
+            
             panel.AddStackedItems(buttonData3, buttonData4, buttonData5);
 
-            //8. Add Split Button
+            
             SplitButtonData splitButtonData = new SplitButtonData("SplitButton", "Split\rButton");
             SplitButton splitButton = panel.AddItem(splitButtonData) as SplitButton;
             splitButton.AddPushButton(buttonData6);
             splitButton.AddPushButton(buttonData7);
 
+            panel.AddSeparator();
+            panel.AddSlideOut();
+            PulldownButtonData pulldownButtonData = new PulldownButtonData("pulldownButton", "Pulldown\rButton");
+//            pulldownButtonData.LargeImage = ConvertToImageSource(byteArray);
 
-
+            PulldownButton pulldownButton = panel.AddItem(pulldownButtonData) as PulldownButton;
+            pulldownButton.AddPushButton(buttonData8);
+            pulldownButton.AddPushButton(buttonData9);
+            pulldownButton.AddPushButton(buttonData10);
 
 
 
@@ -106,7 +125,7 @@ namespace RVTBootcamp_Module04_Challenge
         {
             using (var memoryStream = new MemoryStream())
             {
-                bitmap.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Png); // You can change ImageFormat if needed.
+                bitmap.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Png); //change ImageFormat if needed.
                 return memoryStream.ToArray();
             }
         }
